eb46c466bb0abdd2143b147fd1597fcb7e125e5e
aboytard <alban.boytard@strabag.com>
2026-01-21 17:47:23 +0100

    rename database to participants_sqlite_provider

--- flask-client/controllers/participants_controller.py
+++ flask-client/controllers/participants_controller.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request
-from sqlite.database import init_db, add_participant, get_participants
+from sqlite.participants_sqlite_provider import init_db, add_participant, get_participants
 
 participants_bp = Blueprint('participants', __name__)
 
