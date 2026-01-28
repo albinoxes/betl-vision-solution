be3aa516f886b7f834f810b1d824fb69f3a2ecd2
aboytard <alban.boytard@strabag.com>
2026-01-28 08:59:36 +0100

    move the model-manager to a project setting section

--- flask-client/controllers/ml_model_controller.py
+++ flask-client/controllers/ml_model_controller.py
@@ -27,10 +27,10 @@ def upload_model():
 
     data = file.read()
     ml_provider.insert_model(name, version, model_type, data, category)
-    return redirect(url_for('ml.model_manager'))
+    return redirect(url_for('project.project_settings'))
 
 @ml_bp.route('/delete-model/<int:model_id>', methods=['POST'])
 def delete_model(model_id):
     from sqlite.ml_sqlite_provider import ml_provider
     ml_provider.delete_model_by_id(model_id)
-    return redirect(url_for('ml.model_manager'))
+    return redirect(url_for('project.project_settings'))
